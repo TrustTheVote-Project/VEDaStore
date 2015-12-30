aada430f55687185073fbeef7b893fa823ee9c44
Alex Mekelburg <alex@mekware.com>
2015-12-30 17:22:13 -0500

    fix foreign key ref

--- app/models/vedastore/party_selection.rb
+++ app/models/vedastore/party_selection.rb
@@ -1,7 +1,7 @@
 class Vedastore::PartySelection < Vedastore::BallotSelection
   include XsdRailsFunctions
   
-  has_many :ballot_selection_party_id_refs
+  has_many :ballot_selection_party_id_refs, foreign_key: :ballot_selection_id
   has_many :parties, through: :ballot_selection_party_id_refs
   include Vedaspace::PartySelection
   
